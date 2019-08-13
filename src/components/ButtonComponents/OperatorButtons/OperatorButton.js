c1e06f6cd7bcedd07c972056b27607e6c65c0220
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-08-13 16:01:55 -0700

    got buttons t oappear

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = (props) => {
   return (
     <>
+      <button
+      className="operator-button"
+      onClick={() => props.setOpState(props.opState)}
+    >
+          {props.text}
+          
+      </button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default OperatorButton;
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Numbers from "../ButtonComponents/NumberButtons/Numbers";
+import Operators from "../ButtonComponents/OperatorButtons/Operators";
+import Specials from "../ButtonComponents/SpecialButtons/Specials";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return <div className = "display">{/* Display any props data here */}
+      <Numbers />
+      <Operators />
+      <Specials />
+  </div>;
 };
+
+export default Display;
